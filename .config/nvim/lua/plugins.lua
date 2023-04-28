3aa2f2ec690fcc08a17dcfb23490f99d4a7e003f
louvre2489 <louvre2489@gmail.com>
2023-04-28 09:47:46 +0900

    nvim-metalsを追加

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -641,6 +641,84 @@ return {
     end
   },
   {
+    'scalameta/nvim-metals',
+    ft = { "scala", "sbt" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local api = vim.api
+      local map = vim.keymap.set
+      -- global
+
+      vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+
+      -- LSP mappings
+      map("n", "gD",  vim.lsp.buf.definition)
+      map("n", "K",  vim.lsp.buf.hover)
+      map("n", "gi", vim.lsp.buf.implementation)
+      map("n", "gr", vim.lsp.buf.references)
+      map("n", "gds", vim.lsp.buf.document_symbol)
+      map("n", "gws", vim.lsp.buf.workspace_symbol)
+      map("n", "<leader>cl", vim.lsp.codelens.run)
+      map("n", "<leader>sh", vim.lsp.buf.signature_help)
+      map("n", "<leader>rn", vim.lsp.buf.rename)
+      map("n", "<leader>f", vim.lsp.buf.formatting)
+      map("n", "<leader>ca", vim.lsp.buf.code_action)
+
+      map("n", "<leader>ws", function()
+        require("metals").hover_worksheet()
+      end)
+
+      -- all workspace diagnostics
+      map("n", "<leader>aa", vim.diagnostic.setqflist)
+
+      -- all workspace errors
+      map("n", "<leader>ae", function()
+        vim.diagnostic.setqflist({ severity = "E" })
+      end)
+
+      -- all workspace warnings
+      map("n", "<leader>aw", function()
+        vim.diagnostic.setqflist({ severity = "W" })
+      end)
+
+      -- buffer diagnostics only
+      map("n", "<leader>d", vim.diagnostic.setloclist)
+
+      map("n", "[c", function()
+        vim.diagnostic.goto_prev({ wrap = false })
+      end)
+
+      map("n", "]c", function()
+        vim.diagnostic.goto_next({ wrap = false })
+      end)
+
+      ----------------------------------
+      -- LSP Setup ---------------------
+      ----------------------------------
+      local metals_config = require("metals").bare_config()
+
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      }
+
+      -- Autocmd that will actually be in charging of starting the whole thing
+      local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
+      api.nvim_create_autocmd("FileType", {
+        -- NOTE: You may or may not want java included here. You will need it if you
+        -- want basic Java support but it may also conflict if you are using
+        -- something like nvim-jdtls which also works on a java filetype autocmd.
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
+  },
+  {
     'j-hui/fidget.nvim',
     event = 'BufEnter',
     lazy = true,
